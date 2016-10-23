021485e17b77397d2d04e855a38ffc56f689bbe4
Anthony Naddeo <anthony.naddeo@gmail.com>
2016-10-22 19:48:31 -0700

    Added cross region calls

--- routes/index.js
+++ routes/index.js
@@ -6,26 +6,32 @@ const hamster = new Hamster();
 
 /* GET home page. */
 router.route('/tags').get((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
     hamster.getTags().then(tags => res.status(200).send(tags));
 });
 
 router.route('/activities/:searchTerm?').get((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
     hamster.getActivities(req.searchTerm).then(activities => res.status(200).send(activities));
 });
 
 router.route('/facts/today').get((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
     hamster.getTodaysFacts().then(facts => res.status(200).send(facts));
 });
 
 router.route('/tags/:tagName').post((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
     hamster.getTags().then(tags => res.status(200).send(tags));
 });
 
 router.route('/api').get((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
     Hamster.api().then(api => res.status(200).send(Object.keys(api)));
 });
 
 router.route('*').get((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
     res.status(404).send();
 });
 
